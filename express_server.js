6604cc4c51471e74011ce534840809b8fbcdf800
AD202310 <a.g.dychenko@gmail.com>
2023-11-27 22:54:13 -0500

    Remove unused route

--- express_server.js
+++ express_server.js
@@ -8,14 +8,9 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
-
-// app.get("/urls.json", (req, res) => {
-//   res.json(urlDatabase);
-// });
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
